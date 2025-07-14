2deb5b18ba566d6b40128291f54911f4eb3c4c4d
chamborgir <chamboquilon1@gmail.com>
2025-07-14 18:24:25 +0800

    v9

--- src/StockClient.jsx
+++ src/StockClient.jsx
@@ -3,21 +3,6 @@ import { supabase } from "./supabaseClient";
 
 import "./StockClient.css";
 
-function urlBase64ToUint8Array(base64String) {
-    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-    const base64 = (base64String + padding)
-        .replace(/\-/g, "+")
-        .replace(/_/g, "/");
-
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-}
-
 const StockClient = () => {
     const [allStock, setAllStock] = useState({});
     const [weather, setWeather] = useState(null);
